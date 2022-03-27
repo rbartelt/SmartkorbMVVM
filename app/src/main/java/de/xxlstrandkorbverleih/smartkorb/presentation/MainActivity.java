fb36b127735cf75aa90b128c053122585228a417
rbartelt <robert_bartelt@web.de>
2022-03-27 13:46:02 +0200

    Initial commit

---
+++ app/src/main/java/de/xxlstrandkorbverleih/smartkorb/presentation/MainActivity.java
@@ -0,0 +1,16 @@
+package de.xxlstrandkorbverleih.smartkorb.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import de.xxlstrandkorbverleih.smartkorb.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
